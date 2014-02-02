b531963691bc27907151b1caaed5b4921fc969bc
Anand Biligiri <joywhitehill-github@yahoo.com>
2014-02-02 10:44:12 -0800

    Initialize Firebase Client in App

--- src/com/example/groupmessaging/restapi/GroupMessagingClient.java
+++ src/com/example/groupmessaging/restapi/GroupMessagingClient.java
@@ -2,21 +2,14 @@ package com.example.groupmessaging.restapi;
 
 import android.content.Context;
 
-import com.loopj.android.http.AsyncHttpClient;
+import com.firebase.client.Firebase;
 
-public class GroupMessagingClient extends AsyncHttpClient {
-	private static GroupMessagingClient instance;
-	private Context context;
-
-	public GroupMessagingClient(Context c) {
-		this.context = c;
-	}
+public class GroupMessagingClient {
+	private static Context context;
+	private static Firebase fbClient;
 	
-	public static GroupMessagingClient getInstance(Context c) {
-		if (instance == null) {
-			instance = new GroupMessagingClient(c);
-		}
-		
-		return instance;
+	public static void initialize(Context c, Firebase fb) {
+		context = c;
+		fbClient = fb;
 	}
 }
