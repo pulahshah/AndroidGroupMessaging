1190eb1c54ef830ff335cb0a73c5ce7cbc532ef8
pulah <pulah@yahoo-inc.com>
2014-02-21 22:08:50 -0800

    Chat layout iteration 2: - Always scroll to bottom while opening a chat - Added send button - View scrolls to bottom after new sending new message

--- src/com/example/groupmessaging/activities/MessagesActivity.java
+++ src/com/example/groupmessaging/activities/MessagesActivity.java
@@ -3,13 +3,16 @@ package com.example.groupmessaging.activities;
 import android.app.Activity;
 import android.database.DataSetObserver;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
+import android.widget.Toast;
 
 import com.example.groupmessaging.R;
 import com.example.groupmessaging.adapters.MessageListAdapter;
@@ -24,6 +27,9 @@ public class MessagesActivity extends Activity {
 	public static final String INTENT_PARAM_USERID = "uid";
 	public static final String INTENT_PARAM_GROUPNAME = "gname";
 	
+	String groupID = null;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,7 +38,7 @@ public class MessagesActivity extends Activity {
 		etNewMessage = (EditText)findViewById(R.id.etNewMessage);
 		
 		setTitle(getIntent().getStringExtra(INTENT_PARAM_GROUPNAME));
-		final String groupID = getIntent().getStringExtra(INTENT_PARAM_GROUPID);
+		groupID = getIntent().getStringExtra(INTENT_PARAM_GROUPID);
 		String userID = getIntent().getStringExtra(INTENT_PARAM_USERID);
 		adapter = new MessageListAdapter(groupID, userID, this);
 		
@@ -41,28 +47,37 @@ public class MessagesActivity extends Activity {
 			public void onChanged() {
 				super.onChanged();
 				lvMessages.setSelection(adapter.getCount() - 1);
+				scrollMyListViewToBottom();
 			}
 		});
 		
 		lvMessages.setAdapter(adapter);
-		
-		etNewMessage.setOnEditorActionListener(new OnEditorActionListener() {
-			
-			@Override
-			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-				if (actionId == EditorInfo.IME_ACTION_DONE || actionId == EditorInfo.IME_NULL) {
-					String message = v.getText().toString();
-					if (message != null && message.length() > 0) {
-						GroupMessagingClient.sendMessage(groupID, message);
-						v.setText("");
-					}
-					return true;
-				}
-				
-				return false;
-			}
-		});
 	}
+	
+	public void sendMessage(View v){
+		String message = etNewMessage.getText().toString();
+		if (message != null && message.length() > 0) {
+			if(groupID != null){
+				GroupMessagingClient.sendMessage(groupID, message);
+				scrollMyListViewToBottom();
+				etNewMessage.setText("");
+			}
+		}
+		else{
+			Toast.makeText(getApplicationContext(), "Empty", Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	
+	private void scrollMyListViewToBottom() {
+	    lvMessages.post(new Runnable() {
+	        @Override
+	        public void run() {
+	            lvMessages.setSelection(adapter.getCount() - 1);
+	        }
+	    });
+	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
