f2a4103e0638ba3e98c894b9af602c73c918af7a
Anand Biligiri <joywhitehill-github@yahoo.com>
2014-02-24 21:32:53 -0800

    Change the MapActivity so that it works on the Genymotion
    I changed it to work similar to the Maps Demo we built in class. It is also able to take the screen shot and send the location as text to the group

--- src/com/example/groupmessaging/activities/MapActivity.java
+++ src/com/example/groupmessaging/activities/MapActivity.java
@@ -4,30 +4,45 @@ import java.io.ByteArrayOutputStream;
 
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Context;
+import android.app.Dialog;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.graphics.Bitmap;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.groupmessaging.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends Activity {
+public class MapActivity extends FragmentActivity implements
+	 GooglePlayServicesClient.ConnectionCallbacks,
+	 GooglePlayServicesClient.OnConnectionFailedListener {
 
-	GoogleMap mMap;
-	LocationManager locationManager;
-	LocationListener locationListener;
+	private SupportMapFragment mapFragment;
+	private GoogleMap mMap;
+	private LocationClient mLocationClient;
+	/*
+	 * Define a request code to send to Google Play services This code is
+	 * returned in Activity.onActivityResult
+	 */
+	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+	
 	Location tempLocation;
 	Bitmap localBitmap;
 	byte[] localBytes;
@@ -41,8 +56,164 @@ public class MapActivity extends Activity {
 		ActionBar actionBar = getActionBar();
 		actionBar.setTitle("Share Location");
 
-		loadMap();
-		getCurrentLocation();
+		mLocationClient = new LocationClient(this, this, this);
+		mapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map));
+		
+		mMap = mapFragment.getMap();
+		mMap.setMyLocationEnabled(true);
+		mMap.setBuildingsEnabled(true);
+	}
+	
+	/*
+	 * Called when the Activity becomes visible.
+	 */
+	@Override
+	protected void onStart() {
+		super.onStart();
+		// Connect the client.
+		if (isGooglePlayServicesAvailable()) {
+			mLocationClient.connect();
+		}
+
+	}
+	
+	/*
+	 * Called when the Activity is no longer visible.
+	 */
+	@Override
+	protected void onStop() {
+		// Disconnecting the client invalidates it.
+		mLocationClient.disconnect();
+		super.onStop();
+	}
+	
+	/*
+	 * Handle results returned to the FragmentActivity by Google Play services
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// Decide what to do based on the original request code
+		switch (requestCode) {
+
+		case CONNECTION_FAILURE_RESOLUTION_REQUEST:
+			/*
+			 * If the result code is Activity.RESULT_OK, try to connect again
+			 */
+			switch (resultCode) {
+			case Activity.RESULT_OK:
+				mLocationClient.connect();
+				break;
+			}
+
+		}
+	}
+	
+	private boolean isGooglePlayServicesAvailable() {
+		// Check that Google Play services is available
+		int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		// If Google Play services is available
+		if (ConnectionResult.SUCCESS == resultCode) {
+			// In debug mode, log the status
+			Log.d("Location Updates", "Google Play services is available.");
+			return true;
+		} else {
+			// Get the error dialog from Google Play services
+			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+					CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+			// If Google Play services can provide an error dialog
+			if (errorDialog != null) {
+				// Create a new DialogFragment for the error dialog
+				ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+				errorFragment.setDialog(errorDialog);
+				errorFragment.show(getSupportFragmentManager(), "Location Updates");
+			}
+
+			return false;
+		}
+	}
+	
+	/*
+	 * Called by Location Services when the request to connect the client
+	 * finishes successfully. At this point, you can request the current
+	 * location or start periodic updates
+	 */
+	@Override
+	public void onConnected(Bundle dataBundle) {
+		// Display the connection status
+		Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+		Location location = mLocationClient.getLastLocation();
+		if (location != null) {
+			LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+			CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
+			mMap.animateCamera(cameraUpdate);
+			makeUseOfNewLocation(location);
+		} else {
+			Toast.makeText(this, "Current location was null!", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	/*
+	 * Called by Location Services if the connection to the location client
+	 * drops because of an error.
+	 */
+	@Override
+	public void onDisconnected() {
+		// Display the connection status
+		Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+	}
+
+	/*
+	 * Called by Location Services if the attempt to Location Services fails.
+	 */
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+		/*
+		 * Google Play services can resolve some errors it detects. If the error
+		 * has a resolution, try sending an Intent to start a Google Play
+		 * services activity that can resolve error.
+		 */
+		if (connectionResult.hasResolution()) {
+			try {
+				// Start an Activity that tries to resolve the error
+				connectionResult.startResolutionForResult(this,
+						CONNECTION_FAILURE_RESOLUTION_REQUEST);
+				/*
+				 * Thrown if Google Play services canceled the original
+				 * PendingIntent
+				 */
+			} catch (IntentSender.SendIntentException e) {
+				// Log the error
+				e.printStackTrace();
+			}
+		} else {
+			Toast.makeText(getApplicationContext(),
+					"Sorry. Location services not available to you", Toast.LENGTH_LONG).show();
+		}
+	}
+
+	// Define a DialogFragment that displays the error dialog
+	public static class ErrorDialogFragment extends DialogFragment {
+
+		// Global field to contain the error dialog
+		private Dialog mDialog;
+
+		// Default constructor. Sets the dialog field to null
+		public ErrorDialogFragment() {
+			super();
+			mDialog = null;
+		}
+
+		// Set the dialog to display
+		public void setDialog(Dialog dialog) {
+			mDialog = dialog;
+		}
+
+		// Return a Dialog to the DialogFragment.
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			return mDialog;
+		}
 	}
 
 	@Override
@@ -84,34 +255,11 @@ public class MapActivity extends Activity {
 		finish(); // closes the activity, pass data to parent
 	}
 
-
-	private void getCurrentLocation() {
-		// Acquire a reference to the system Location Manager
-		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-		// Define a listener that responds to location updates
-		locationListener = new LocationListener() {
-			public void onLocationChanged(Location location) {
-				// Called when a new location is found by the network location provider.
-				tempLocation = location;
-				makeUseOfNewLocation(location);
-			}
-
-			public void onStatusChanged(String provider, int status, Bundle extras) {}
-
-			public void onProviderEnabled(String provider) {}
-
-			public void onProviderDisabled(String provider) {}
-		};
-
-		// Register the listener with the Location Manager to receive location updates
-		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-	}
-
 	public void makeUseOfNewLocation(Location l){
 		LatLng latlon = new LatLng(l.getLatitude(), l.getLongitude());
 
 		Log.d("DEBUG", "New location: " + l.getLatitude() + " " + l.getLongitude());
+		tempLocation = l;
 		CameraPosition cameraPosition = new CameraPosition.Builder()
 		.target(latlon)      // Sets the center of the map to location
 		.zoom(17)                   // Sets the zoom
@@ -120,9 +268,6 @@ public class MapActivity extends Activity {
 		.build();                   // Creates a CameraPosition from the builder
 		mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 		mMap.addMarker(new MarkerOptions().position(latlon));
-
-
-		locationManager.removeUpdates(locationListener);
 
 		mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
 			public void onMapLoaded() {
@@ -140,24 +285,6 @@ public class MapActivity extends Activity {
 				});
 			}
 		});
-
-	}
-
-	private void loadMap(){
-
-		// Do a null check to confirm that we have not already instantiated the map.
-		if (mMap == null) {
-			mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-					.getMap();
-			// Check if we were successful in obtaining the map.
-			if (mMap != null) {
-				// The Map is verified. It is now safe to manipulate the map.
-
-			}
-		}
-		mMap.setMyLocationEnabled(true);
-		mMap.setBuildingsEnabled(true);
-
 
 	}
 
